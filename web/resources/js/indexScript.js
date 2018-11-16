47444fa81ff8a623de4b7ef1920f083e7f9d1ec4
Purevdorj <purvee.hq@gmail.com>
2018-11-16 15:08:13 -0600

    Index Page (html js css only)

---
+++ web/resources/js/indexScript.js
@@ -0,0 +1,49 @@
+$(function () {
+    $('.post-list .mdl-list__item').click(listItemClickHandler);
+});
+
+function listItemClickHandler() {
+    // unfocus focused element;
+    $(this).blur();
+
+    let bound = this.getBoundingClientRect();
+    console.log(this, bound);
+
+    // console.log($(this));
+    createModal({
+        'top': bound.y + bound.height / 2 + 'px',
+        'left': bound.x + bound.width / 2 + 'px',
+        'width': bound.width + 'px',
+        'height': bound.height + 'px'
+    });
+}
+
+function createModal(initialCSS) {
+    let modal = $('<div>', {
+        class: 'modal'
+    });
+
+    let modalContent = $('<div>', {
+        class: 'modalContent'
+    });
+
+    let loader = $('<div>', {
+        class: ['mdl-spinner mdl-js-spinner is-active']
+    });
+
+    modal.append(modalContent);
+    modalContent.append(loader);
+    modalContent.css(initialCSS);
+
+    componentHandler.upgradeElement(loader[0]);
+
+    modal.click(function () {
+        $(this).remove();
+    });
+    modalContent.click(function (e) {
+        e.stopPropagation();
+    });
+
+    $('body').append(modal);
+    modal.show();
+}
